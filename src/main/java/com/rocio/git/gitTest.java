31de62a816c69c32f65a93971dd1eebf948600ce
rocio <Rocio@qq.com>
2022-10-13 17:16:42 +0800

    first commit

---
+++ src/main/java/com/rocio/git/gitTest.java
@@ -0,0 +1,10 @@
+package com.rocio.git;
+/**
+ * @author cici
+ * @version 1.0
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
