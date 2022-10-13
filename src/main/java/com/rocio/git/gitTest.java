a0ed69a27141e49302a99ff0bd703c1c6b5a41c5
rocio <Rocio@qq.com>
2022-10-13 17:28:59 +0800

    hot-fix commit2

--- src/main/java/com/rocio/git/gitTest.java
+++ src/main/java/com/rocio/git/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 2.0");
+        System.out.println("hot-fix test");
     }
 }
